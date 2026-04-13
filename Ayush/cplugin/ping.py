c5ef2624ea9d216da21a759f2923206e7e7d48bb
tmmteam <teamtmm2@gmail.com>
2026-04-13 09:16:38 +0530

    🥀ᴀʏᴜsʜ🥀

--- Ayush/cplugin/ping.py
+++ Ayush/cplugin/ping.py
@@ -7,8 +7,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import SUPPORT_CHAT, PING_IMG_URL
 from .utils import StartTime
-from AyushMusic.utils import get_readable_time
-from AyushMusic.utils.database.clonedb import get_owner_id_from_db, get_cloned_support_chat, get_cloned_support_channel
+from Ayush.utils import get_readable_time
+from Ayush.utils.database.clonedb import get_owner_id_from_db, get_cloned_support_chat, get_cloned_support_channel
 
 
 @Client.on_message(filters.command("ping"))
